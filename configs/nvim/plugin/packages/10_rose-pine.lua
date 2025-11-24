90b4b26f60ea6ab40474677a9c5d69b26ec6005e
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-11-24 22:32:48 +0200

    fix: rose-pine installation

--- configs/nvim/plugin/packages/10_rose-pine.lua
+++ configs/nvim/plugin/packages/10_rose-pine.lua
@@ -1,5 +1,5 @@
 require("utils").add_package(
-  { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
+  { { src = "https://github.com/rose-pine/neovim", name = "rose-pine", }, },
   function()
     require("rose-pine").setup({
       variant = "dawn", -- auto, main, moon, or dawn
