0d7c9f54376c0b0b758fd787521f683fcb40b33e
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-12-03 09:54:50 +0100

    fix: point password store to correct place

--- configs/home/pass/pass.nix
+++ configs/home/pass/pass.nix
@@ -3,5 +3,12 @@
   programs.password-store = {
     enable = true;
     package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
+    settings = {
+      PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
+    };
   };
+
+  programs.nushell.extraEnv = ''
+    $env.PASSWORD_STORE_DIR = $"($env.HOME)/.local/share/password-store"
+  '';
 }
