74dfc133bba1a2147b54c2850e85f265640da390
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-12-22 22:52:27 +0200

    feat: simplify dev-env monorepo

--- envs/monorepo-3.0/flake.nix
+++ envs/monorepo-3.0/flake.nix
@@ -1,4 +1,9 @@
 {
   inputs.dev-env.url = "path:../../lib/dev-env";
-  outputs = { dev-env, ... }: dev-env.lib.mkEnv [ "nodejs_20" ];
+  outputs =
+    { dev-env, ... }:
+    dev-env.lib.mkEnv [
+      "nodejs_20"
+      "pnpm_8"
+    ];
 }
