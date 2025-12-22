5e949e70d933cb2759afc86dd79816a70a5be992
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-12-22 12:54:03 +0200

    chore: add path block to extraEnv for clarity

--- configs/home/nushell/nushell.nix
+++ configs/home/nushell/nushell.nix
@@ -24,6 +24,16 @@ in
   programs.nushell = {
     enable = true;
     extraConfig = coreConfig + "\n" + allIntegrations;
+    extraEnv = ''
+      $env.PATH = (
+        $env.PATH | split row (char esep)
+        | prepend $"($env.HOME)/.local/bin"
+        | prepend $"/etc/profiles/per-user/($env.USER)/bin"
+        | prepend "/run/current-system/sw/bin"
+        | prepend "/nix/var/nix/profiles/default/bin"
+        | uniq
+      )
+    '';
     environmentVariables = {
       SHELL = lib.getExe pkgs.nushell;
     };
--- configs/home/nushell/nushell.nu
+++ configs/home/nushell/nushell.nu
@@ -1,12 +1,3 @@
-$env.PATH = (
-  $env.PATH | split row (char esep)
-  | prepend $"($env.HOME)/.local/bin"
-  | prepend $"/etc/profiles/per-user/($env.USER)/bin"
-  | prepend "/run/current-system/sw/bin"
-  | prepend "/nix/var/nix/profiles/default/bin"
-  | uniq
-)
-
 $env.config.keybindings ++= [
   {
     name: completion_menu
