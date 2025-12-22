7373109d68887ba038aac7021e9094761896e6b4
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-12-22 17:43:09 +0200

    chore: add simple-client env

---
+++ envs/simple-client/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Dev environment for simple-client";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_20
+              pkgs.pnpm_8
+            ];
+          };
+        }
+      );
+    };
+}
