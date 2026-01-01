abe3093ce2d6fca4a92e9fc7315a0bebf361d890
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-01-01 16:23:14 +0200

    feat: remove zellij

--- configs/home/alacritty/alacritty.nix
+++ configs/home/alacritty/alacritty.nix
@@ -1,9 +1,24 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   catppuccin.alacritty.enable = true;
   programs.alacritty = {
     enable = true;
     settings = {
+      env = {
+        # XDG environment variables for Nushell.
+        # Nix's xdg setup only works for zsh and bash, so we need to set these manually.
+        HOME = config.home.homeDirectory;
+        XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
+        XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
+        XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
+        XDG_STATE_HOME = "${config.home.homeDirectory}/.local/state";
+        XDG_BIN_HOME = "${config.home.homeDirectory}/.local/bin"; # not standard, but useful
+      };
       font = {
         normal = {
           family = "JetBrainsMono Nerd Font";
@@ -20,11 +35,7 @@
         size = 14;
       };
       terminal.shell = {
-        program = lib.getExe pkgs.zellij;
-        args = [
-          "-l"
-          "welcome"
-        ];
+        program = lib.getExe pkgs.nushell;
       };
       window.decorations = "Transparent";
       window.startup_mode = "Fullscreen";
