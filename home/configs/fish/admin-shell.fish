9f32855493ac7d8f674fbc3537a442c2e0d4b544
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-03-20 22:38:22 +0200

    fix(admin-shell): pass DEVENV_ROOT to admin shell env

--- home/configs/fish/admin-shell.fish
+++ home/configs/fish/admin-shell.fish
@@ -8,4 +8,11 @@ if set -q LANG
     set lang $LANG
 end
 
-/usr/bin/env -i TERM="$term" LANG="$lang" /usr/bin/su -l otahontas-admin
+set -l devenv_root ""
+if set -q DEVENV_ROOT
+    set devenv_root $DEVENV_ROOT
+else
+    set devenv_root (command git rev-parse --show-toplevel 2>/dev/null)
+end
+
+/usr/bin/env -i TERM="$term" LANG="$lang" DEVENV_ROOT="$devenv_root" /usr/bin/su -l otahontas-admin
