6212ac68d081640fe2a4860a2e146f3b1e7f4c0c
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-03-20 13:13:27 +0200

    feat(iina): set as default handler for wav and mp3 files

--- home/configs/iina/default.nix
+++ home/configs/iina/default.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.packages = [ pkgs.iina ];
+  home.packages = [
+    pkgs.iina
+    pkgs.duti
+  ];
+
+  home.activation.iinaFileAssociations = lib.hm.dag.entryAfter [ "copyApps" ] ''
+    ${pkgs.duti}/bin/duti -s com.colliderli.iina .wav all
+    ${pkgs.duti}/bin/duti -s com.colliderli.iina .mp3 all
+  '';
 }
