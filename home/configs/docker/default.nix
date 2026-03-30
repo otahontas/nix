f42e43da64f17cee805cfab0ddf7460967e25170
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-03-30 13:49:14 +0300

    chore: remove firefox, orbstack, spotify, syncthing from nix

--- home/configs/docker/default.nix
+++ home/configs/docker/default.nix
@@ -1,19 +1,11 @@
 {
-  config,
   lib,
   pkgs,
   ...
 }:
 {
   home = {
-    # must install lower priority so orbstack's docker-credential-osxkeychain wins
-    # this is needed still so docker + password-store helper works
     packages = [ (lib.lowPrio pkgs.docker-credential-helpers) ];
     file.".docker/config.json".source = ./config.json;
-    # point docker vars to orbstack so testcontainers works properly
-    sessionVariables = {
-      DOCKER_HOST = "unix://${config.home.homeDirectory}/.orbstack/run/docker.sock";
-      TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE = "/var/run/docker.sock";
-    };
   };
 }
--- home/configs/syncthing/default.nix
+++
@@ -1,3 +0,0 @@
-_: {
-  services.syncthing.enable = true;
-}
