249eddc82f91ae166ce3aa3a55212817b3c7f25b
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-03-31 19:28:55 +0300

    feat: add nix-managed symlinks from Music to Documents for iCloud sync

---
+++ home/configs/link-symlinks/default.nix
@@ -0,0 +1,13 @@
+{ config, ... }:
+{
+  home.file = {
+    "Music/Audio Music Apps".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Audio Music Apps";
+    "Music/Logic".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Logic";
+    "Music/MainStage".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/MainStage";
+    "Music/MuseScore4".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/MuseScore4";
+  };
+}
