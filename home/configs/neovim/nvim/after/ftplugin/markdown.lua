4e2dfeb0ed0b67ee22cb6008e4e5ee6f097de599
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-04-02 23:06:24 +0300

    feat(neovim): support toggling multiple checkboxes via visual selection

--- home/configs/neovim/nvim/after/ftplugin/markdown.lua
+++ home/configs/neovim/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,6 @@
 require("utils").setup_prose_buffer()
 
-local function toggle_checkbox()
-	local line = vim.api.nvim_get_current_line()
+local function toggle_line(line)
 	local new_line = line
 
 	if line:match("%[ %]") then
@@ -25,9 +24,41 @@ local function toggle_checkbox()
 		end
 	end
 
-	if new_line ~= line then
-		vim.api.nvim_set_current_line(new_line)
+	return new_line
+end
+
+local function toggle_checkbox_range(start_line, end_line)
+	local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+	local new_lines = {}
+	local changed = false
+	for i, line in ipairs(lines) do
+		local new_line = toggle_line(line)
+		new_lines[i] = new_line
+		if new_line ~= line then
+			changed = true
+		end
+	end
+	if changed then
+		vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, new_lines)
 	end
 end
 
+local function toggle_checkbox()
+	local mode = vim.fn.mode()
+	local start_line, end_line
+
+	if mode == "v" or mode == "V" or mode == "\22" then
+		-- Visual mode: exit visual to set '< '> marks, then use them
+		vim.cmd("normal! <Esc>")
+		start_line = vim.fn.line("'<")
+		end_line = vim.fn.line("'>")
+	else
+		start_line = vim.api.nvim_win_get_cursor(0)[1]
+		end_line = start_line
+	end
+
+	toggle_checkbox_range(start_line, end_line)
+end
+
 vim.keymap.set("n", "<leader>xx", toggle_checkbox, { buffer = true, desc = "Toggle markdown checkbox" })
+vim.keymap.set("x", "<leader>xx", toggle_checkbox, { buffer = true, desc = "Toggle markdown checkboxes in selection" })
