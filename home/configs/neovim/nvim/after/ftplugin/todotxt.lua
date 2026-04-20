7da85b371e80bf01f646c36912c20e9689007fa3
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-04-20 22:04:02 +0300

    fix(neovim): replace commentstring-based todo.txt toggle with leader xx that adds/removes done date

--- home/configs/neovim/nvim/after/ftplugin/todotxt.lua
+++ home/configs/neovim/nvim/after/ftplugin/todotxt.lua
@@ -1,8 +1,54 @@
 local utils = require("utils")
 utils.setup_prose_buffer()
 
--- toggle done with commentstring
-vim.bo.commentstring = "x %s"
+-- Toggle done/undone with <leader>xx (like markdown checkbox toggle)
+local function toggle_done_line(line)
+	local today = os.date("%Y-%m-%d")
+	local leading = line:match("^(%s*)") or ""
+	local rest = line:match("^%s*(.*)") or ""
+	-- Already done: remove "x YYYY-MM-DD " prefix (after leading whitespace)
+	local done_prefix = "^x (%d%d%d%d%-%d%d%-%d%d) "
+	if rest:match(done_prefix) then
+		return leading .. rest:gsub(done_prefix, "")
+	end
+	-- Not done: prepend "x YYYY-MM-DD "
+	return leading .. "x " .. today .. " " .. rest
+end
+
+local function toggle_done_range(start_line, end_line)
+	local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+	local new_lines = {}
+	local changed = false
+	for i, line in ipairs(lines) do
+		local new_line = toggle_done_line(line)
+		new_lines[i] = new_line
+		if new_line ~= line then
+			changed = true
+		end
+	end
+	if changed then
+		vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, new_lines)
+	end
+end
+
+local function toggle_done()
+	local mode = vim.fn.mode()
+	local start_line, end_line
+
+	if mode == "v" or mode == "V" or mode == "\22" then
+		vim.cmd("normal! <Esc>")
+		start_line = vim.fn.line("'<")
+		end_line = vim.fn.line("'>")
+	else
+		start_line = vim.api.nvim_win_get_cursor(0)[1]
+		end_line = start_line
+	end
+
+	toggle_done_range(start_line, end_line)
+end
+
+vim.keymap.set("n", "<leader>xx", toggle_done, { buffer = true, desc = "Toggle todo.txt done" })
+vim.keymap.set("x", "<leader>xx", toggle_done, { buffer = true, desc = "Toggle todo.txt done (visual)" })
 
 -- Date patterns
 local PATTERNS = {
